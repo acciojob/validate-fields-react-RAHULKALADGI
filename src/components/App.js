e5f754d81aef7ee7c4f7d0993cb62089029f7330
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-07-01 18:29:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,26 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+const App = ()=> {
+    let [name , setName] = useState("");
+    let [pass, setPass] = useState("");
+    let [error , setError] = useState("");
+
+    function func () {
+        if(name=="" || pass=="") {
+            setError("Both username and password are required")
+        }
+    }
+    return (
+        <div>
+            <label for="name">Username:</label>
+            <input type="text" onChange={(event)=>setName(event.target.value)} />
+            <label for="password">Password:</label>
+            <input type="password" onChange={(event)=>setPass(event.target.value)}/>
+            <button onClick={func}>Login</button>
+            <p id="errorMessage">{error}</p>
+        </div>
+    )
 }
 
 export default App
